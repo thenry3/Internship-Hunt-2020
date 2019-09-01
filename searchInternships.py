310bd99d847ec6e4adea688269fa37f740a977a2
thenry3 <43651306+thenry3@users.noreply.github.com>
2019-09-01 01:51:31 -0700

    redstructed test

--- searchInternships.py
+++ searchInternships.py
@@ -4,11 +4,16 @@ from sendAlert import sendSMS
 import schedule
 import time
 
-schedule.every().day.at("06:00").do(scrapeGithub)
 
-while True:
-    schedule.run_pending()
+def runCheck():
+    scrapeGithub()
     if subprocess.getoutput('diff internships2020.csv internships2020-1.csv') != "":
         sendSMS()
     subprocess.run(['cp', "internships2020-1.csv", "internships2020.csv"])
+
+
+schedule.every().day.at("06:00").do(runCheck)
+
+while True:
+    schedule.run_pending()
     time.sleep(0.5)
